33c99e1e24992fe28c907cc6a07153fa65c3a5f8
Audran Audran <santos.audran@gmail.com>
2025-11-20 10:05:11 +0100

    Ajout gestion financière des projets

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from technique import views as technique_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +14,23 @@ urlpatterns = [
     path('gestion-des-candidatures/', include('recrutement.urls')),
     path('pole-technique/documents/', include('technique.urls')),
     path('signatures/', include('signatures.urls')),
-    path('', include('management.urls')),
+    # Module financier partie technique
+    path(
+    "pole-technique/vue-financiere/",
+    technique_views.financial_overview,
+    name="technique_financial_overview",
+    ),
+    path(
+    "pole-technique/vue-financiere/<int:pk>/",
+    technique_views.financial_project_detail,
+    name="technique_financial_project_detail",
+    ),
+    path(
+    "pole-technique/vue-financiere/<int:pk>/pdf/",
+    technique_views.financial_project_pdf,
+    name="technique_financial_project_pdf",
+    ),
+    
 ]
 
 if settings.DEBUG:
--- technique/forms.py
+++ technique/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import DocumentTechnique
+from .models import DocumentTechnique, TechnicalProject
 
 
 class DocumentTechniqueUploadForm(forms.ModelForm):
@@ -11,3 +11,19 @@ class DocumentTechniqueUploadForm(forms.ModelForm):
                 attrs={"accept": ".pdf,.doc,.docx,.txt"}
             )
         }
+
+
+class TechnicalProjectCreateForm(forms.ModelForm):
+    """Créer un projet"""
+
+    class Meta:
+        model = TechnicalProject
+        fields = ["name", "reference", "total_estimated"]
+
+
+class TechnicalProjectFinanceForm(forms.ModelForm):
+    """Modifier les données financières d'un projet"""
+
+    class Meta:
+        model = TechnicalProject
+        fields = ["engaged_amount", "paid_amount", "total_estimated"]
